f87af2ac321628f8e0d02539bf91d52b89dcaeb6
EllyAtl <elly.attal@centrale.centralelille.fr>
2026-05-05 03:21:30 +0200

    Add instance generator with connected graph

--- ammm-main/InstanceGenerator/InstanceGenerator.py
+++ ammm-main/InstanceGenerator/InstanceGenerator.py
@@ -1,21 +1,6 @@
 '''
-AMMM P2 Instance Generator v2.0
-Instance Generator class.
-Copyright 2020 Luis Velasco.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+AMMM Project Heuristics
+Instance generator.'''
 
 import os, random
 from AMMMGlobals import AMMMException
@@ -33,13 +18,18 @@ class InstanceGenerator(object):
         fileNameExtension = self.config.fileNameExtension
         numInstances = self.config.numInstances
 
-        numCPUs = self.config.numCPUs
-        minCapacityPerCPU = self.config.minCapacityPerCPU
-        maxCapacityPerCPU = self.config.maxCapacityPerCPU
+        numBases = self.config.numBases
+        minHours = self.config.minHours
+        maxHours = self.config.maxHours
 
-        numTasks = self.config.numTasks
-        minResourcesPerTask = self.config.minResourcesPerTask
-        maxResourcesPerTask = self.config.maxResourcesPerTask
+        numSpecialists = self.config.numSpecialists
+        minCapacity = self.config.minCapacity
+        maxCapacity = self.config.maxCapacity
+        minFee = self.config.minFee
+        maxFee = self.config.maxFee
+
+        if not os.path.isdir(instancesDirectory):
+            os.makedirs(instancesDirectory)
 
         if not os.path.isdir(instancesDirectory):
             raise AMMMException('Directory(%s) does not exist' % instancesDirectory)
@@ -48,19 +38,43 @@ class InstanceGenerator(object):
             instancePath = os.path.join(instancesDirectory, '%s_%d.%s' % (fileNamePrefix, i, fileNameExtension))
             fInstance = open(instancePath, 'w')
 
-            cpuCapacity = [0] * numCPUs
-            for c in range(numCPUs):
-                cpuCapacity[c] = random.uniform(minCapacityPerCPU, maxCapacityPerCPU)
+            specialistCapacity = [0] * numSpecialists
+            for c in range(numSpecialists):
+                specialistCapacity[c] = random.randint(minCapacity, maxCapacity)
 
-            taskResources = [0] * numTasks
-            for t in range(numTasks):
-                taskResources[t] = random.uniform(minResourcesPerTask, maxResourcesPerTask)
+            specialistFee = [0] * numSpecialists
+            for t in range(numSpecialists):
+                specialistFee[t] = random.randint(minFee, maxFee)
 
-            fInstance.write('nTasks=%d;\n' % numTasks)
-            fInstance.write('nCPUs=%d;\n' % numCPUs)
+            pipeHours = [[-1 for _ in range(numBases)] for _ in range(numBases)]
+            for a in range(numBases):
+                pipeHours[a][a] = 0
+
+            for b in range(1, numBases):
+                value = random.randint(minHours, maxHours)
+                pipeHours[b][b - 1] = value
+                pipeHours[b - 1][b] = value
+
+            for k in range(numBases):
+                for r in range(k + 1, numBases):
+                    if pipeHours[k][r] == -1 and random.random() < 0.8 :
+                        value = random.randint(minHours, maxHours)
+                        pipeHours[k][r] = value
+                        pipeHours[r][k] = value
+
+            fInstance.write('numBases=%d;\n' % numBases)
+            fInstance.write('numSpecialists=%d;\n' % numSpecialists)
 
             # translate vector of floats into vector of strings and concatenate that strings separating them by a single space character
-            fInstance.write('rt=[%s];\n' % (' '.join(map(str, taskResources))))
-            fInstance.write('rc=[%s];\n' % (' '.join(map(str, cpuCapacity))))
+            fInstance.write('c=[%s];\n' % (' '.join(map(str, specialistFee))))
+            fInstance.write('w=[%s];\n' % (' '.join(map(str, specialistCapacity))))
+            fInstance.write('t=[\n')
+            for row_idx, row in enumerate(pipeHours):
+                row_str = ' '.join(map(str, row))
+                if row_idx < len(pipeHours) - 1:
+                    fInstance.write('  [%s],\n' % row_str)
+                else:
+                    fInstance.write('  [%s]\n' % row_str)
+            fInstance.write('];\n')
 
             fInstance.close()
